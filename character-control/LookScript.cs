00d2effdf7c617236e07ea1ee510c8c81cee9136
conner.root <conner.root@mymail.champlain.edu>
2020-01-18 21:01:23 -0500

    Added shaders for grayscale stuff, and basic player control

---
+++ character-control/LookScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* *
+ * NOT COMPLETELY MINE
+ * https://answers.unity.com/questions/29741/mouse-look-script.html
+ * https://forum.unity.com/threads/mouse-look-script.233903/
+ * */
+
+public class LookScript : MonoBehaviour
+{
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	[Range(0f, 100f)]
+	public float sensitivity = 10.0f;
+
+	[Header("Look Bounds")]
+	public float minXRot = -360f;
+	public float maxXRot = 360f;
+	public float minYRot = -60f;
+	public float maxYRot = 60f;
+
+	float rot_X, rot_Y;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Locks cursor in center of screen
+		Cursor.lockState = CursorLockMode.Locked;
+
+		// Get initial rotation
+		Vector3 euler = transform.rotation.eulerAngles;
+		
+		// Set initial rotation
+		rot_X = euler.x;
+		rot_Y = euler.y;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Only update required axis
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			UpdateXRotation();
+			UpdateYRotation();
+		}
+		else if (axes == RotationAxes.MouseX)
+			UpdateXRotation();
+		else
+			UpdateYRotation();
+
+		// Set new rotation
+		transform.rotation = Quaternion.Euler(rot_Y, rot_X, 0.0f);
+	}
+
+	void UpdateXRotation()
+	{
+		// Update x rotation
+		rot_X += Input.GetAxis("Mouse X") * (sensitivity * Time.deltaTime);
+		
+		// Wrap rotation
+		if (rot_X < minXRot)
+			rot_X += maxXRot;
+		else if (rot_X > maxXRot)
+			rot_X -= maxXRot;
+	}
+
+	void UpdateYRotation()
+	{
+		// Update y rotation
+		rot_Y -= Input.GetAxis("Mouse Y") * (sensitivity * Time.deltaTime);
+		
+		// Clamp rotation
+		if (rot_Y < minYRot)
+			rot_Y = minYRot;
+		else if (rot_Y > maxYRot)
+			rot_Y = maxYRot;
+	}
+}
---
+++ character-control/PlayerMoveScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+	public float speed = 10.0f;
+	public float jump = 10.0f;
+	
+	private Rigidbody rb;
+	private float xMove;
+	private float yMove;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GetInput();
+		Move();
+	}
+
+	void GetInput()
+	{
+		xMove = Input.GetAxis("Horizontal");
+		yMove = Input.GetAxis("Vertical");
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			Jump();
+	}
+
+	void Move()
+	{
+		float fall = rb.velocity.y;
+		Vector3 dir = transform.forward * yMove + transform.right * xMove;
+
+		dir.Normalize();
+		dir *= speed;
+		dir.y = fall;
+
+		rb.velocity = dir;
+
+		rb.angularVelocity = Vector3.zero;
+	}
+
+	void Jump()
+	{
+		rb.AddForce(transform.up * jump);
+	}
+}
